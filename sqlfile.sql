37c40d185561197556eb3b06b6843fe68c3d7419
Cozg <cozg@gmail.com>
2025-11-06 14:00:13 +0700

    update

---
+++ sqlfile.sql
@@ -0,0 +1,9 @@
+﻿-- Code trong file sql đang lưu tại D:\QTHCSDL\sqlfile.sql
+
+USE Northwind  
+GO  
+SELECT Country AS QuocGia, COUNT(CustomerID) AS SoLuongKhachHang   
+FROM Customers  
+GROUP BY Country
+ORDER BY COUNT(CustomerID) DESC
+GO  
